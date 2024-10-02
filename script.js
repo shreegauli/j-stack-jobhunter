b3d0b9bf4d300459184527e3ddd73d2abd7ef96b
Shree Krishna Gauli <shreegauli1@gmail.com>
2024-10-02 20:26:15 +0545

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+const bar = document.getElementById("bar");
+const menu = document.getElementById("menue");
+
+if(bar){
+    bar.addEventListener("click",()=>{
+        menu.classList.toggle("active");
+    });
+}
+const baar=document.querySelector("#bar")
+console.log(baar)
