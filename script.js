331b722b219b73ba6cf28c7cf71d2e89636ee265
Shree Krishna Gauli <shreegauli1@gmail.com>
2024-10-03 12:29:25 +0545

    changes in html css and scriptjs

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ if(bar){
 }
 const baar=document.querySelector("#bar")
 console.log(baar)
+
+const sortBtns = document.querySelectorAll(".job-id > *");
+
+sortBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        // Remove "active" class from all buttons
+        sortBtns.forEach((btn) => btn.classList.remove("active"));
+        // Add "active" class to the clicked button
+        btn.classList.add("active");
+    });
+});
