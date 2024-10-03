073ebcf18e7a9e59417bff43e8ecf6f4178819e3
Shree Krishna Gauli <shreegauli1@gmail.com>
2024-10-03 14:57:01 +0545

    Added user page and make some changes

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
+
 const bar = document.getElementById("bar");
 const menu = document.getElementById("menue");
 
-if(bar){
-    bar.addEventListener("click",()=>{
+if (bar) {
+    bar.addEventListener("click", () => {
         menu.classList.toggle("active");
     });
 }
-const baar=document.querySelector("#bar")
-console.log(baar)
+
+document.addEventListener("click", (event) => {
+    if (!menu.contains(event.target) && !bar.contains(event.target)) {
+        menu.classList.remove("active");
+    }
+});
+
+const baar = document.querySelector("#bar");
+console.log(baar);
 
 const sortBtns = document.querySelectorAll(".job-id > *");
 
 sortBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
-        // Remove "active" class from all buttons
         sortBtns.forEach((btn) => btn.classList.remove("active"));
-        // Add "active" class to the clicked button
         btn.classList.add("active");
     });
 });
